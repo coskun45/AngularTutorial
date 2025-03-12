55025a0baa4360155d792ffac68ee6f8b98668c0
Eyüp <coskuney85480@th-nuernberg.de>
2025-03-12 12:09:04 +0100

    structure directive

--- proj1/src/app/app.component.ts
+++ proj1/src/app/app.component.ts
@@ -4,17 +4,42 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `
-    <h1>{{title}}</h1> <!-- text interpolation -->
-    <input type="text" [value]="title" /> <!-- property binding -->
+    <!-- ngfor directive -->
+    <ul>
+      <li *ngFor="let item of names; 
+      let index = index;
+      let first = first;
+      let last = last;
+      let odd = odd;
+      let even = even;
 
-    <app-home [pageName]="title"></app-home>
+      
+      ">
+      
+      {{index}}-{{item}}</li>
+    </ul>
 
-    <button (click)="buttonClick()">Test Button</button> <!-- event binding -->
-    <br>
-   <!-- two way binding -->
-    <input type="text" [(ngModel)]="name" (ngModelChange)="onChange($event)"/> <br>
-    <input type="text" [(ngModel)]="name" /> <br>
-    <p>{{name}}</p>
+    <!-- ngif directive -->
+    <div *ngIf="names.length > 30; else elseBlock">
+  <h1>There are more than 3 names</h1>
+</div>
+<ng-template #elseBlock>
+  <h1>There are 3 or fewer names</h1>
+</ng-template>
+
+<!-- ngswitch directive -->
+<div [ngSwitch]="names.length">
+  <div *ngSwitchCase="1">One</div>
+  <div *ngSwitchCase="2">Two</div>
+  <div *ngSwitchCase="3">Three</div>
+  <div *ngSwitchCase="4">Four</div>
+  <div *ngSwitchCase="5">Five</div>
+  <div *ngSwitchDefault>More than five</div>
+</div>
+
+<!-- ngstyle directive -->
+<div [ngStyle]="{'font-style': styleExp ? 'italic' : 'normal'}">This is italic</div>
+<button (click)="styleExp = !styleExp">Toggle style</button>  
 
 
      
@@ -23,14 +48,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Eyüp Coskun';
-  name: string;
-
-  buttonClick(){
-    alert('Button clicked');
-  }
-
-  onChange(event){
-    console.log(event);
-  }
+  names: string[] = ['John', 'Doe', 'Smith', 'Brown', 'Johnson', 'ali'];
 }
