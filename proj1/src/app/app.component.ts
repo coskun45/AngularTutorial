847fb53e315340a69b5bd3d17ca2b9de3e543d00
Eyüp <coskuney85480@th-nuernberg.de>
2025-03-12 12:11:07 +0100

    initial commit

--- proj1/src/app/app.component.ts
+++ proj1/src/app/app.component.ts
@@ -4,17 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `
-    <h1>{{title}}</h1> <!-- text interpolation -->
-    <input type="text" [value]="title" /> <!-- property binding -->
-
-    <app-home [pageName]="title"></app-home>
-
-    <button (click)="buttonClick()">Test Button</button> <!-- event binding -->
-    <br>
-   <!-- two way binding -->
-    <input type="text" [(ngModel)]="name" (ngModelChange)="onChange($event)"/> <br>
-    <input type="text" [(ngModel)]="name" /> <br>
-    <p>{{name}}</p>
+  <h1>{{title}}</h1>
 
 
      
@@ -23,14 +13,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Eyüp Coskun';
-  name: string;
-
-  buttonClick(){
-    alert('Button clicked');
-  }
-
-  onChange(event){
-    console.log(event);
-  }
+  title = 'proj1';
 }
