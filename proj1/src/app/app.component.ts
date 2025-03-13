0c8c704a58811da7894af55123f76dac11e51b27
Eyüp <coskuney85480@th-nuernberg.de>
2025-03-13 15:34:45 +0100

    model driven/reactive form

--- proj1/src/app/app.component.ts
+++ proj1/src/app/app.component.ts
@@ -1,17 +1,57 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `
-  <h1>{{title}}</h1>
+  <form [formGroup]="frm" (ngSubmit)="onSubmit()">
+    <input type="text" placeholder="Name" formControlName="name"><br>
+    <input type="text" placeholder="Surname" formControlName="surname"><br>
+    <input type="text" placeholder="Email" formControlName="email"><br>
+    <input type="text" placeholder="Tel" formControlName="tel"><br>
 
+    <div formGroupName="address">
+    <input type="text" placeholder="Country" formControlName="country"><br>
+    <input type="text" placeholder="City" formControlName="city"><br>
+    <input type="text" placeholder="Adress" formControlName="address"><br>
 
-     
+    </div>
+    <button>Send</button>
+  </form>
+  <button (click)="ok()">Ok</button>
+
+     Valid: {{frm.valid}}
     
     `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'proj1';
+  frm:FormGroup;
+  constructor(private formBuilder:FormBuilder){
+    this.frm = formBuilder.group({
+      name:["", Validators.required],
+      surname:[""],
+      email:[""],
+      tel:[""],
+
+      address: formBuilder.group({
+        country:[""],
+        city:[""],
+        adress:[""]
+      })
+    })
+    this.frm.valueChanges.subscribe({
+      next:data =>{
+        console.log(data)
+      }
+    })
+  }
+
+  onSubmit(){
+    console.log(this.frm.value)
+  }
+  ok(){
+    this.frm.get('name').setValue("coskun",{onlySelf:true})
+  }
 }
